88079bb400288cc298a04cd59d49ba544a258207
Agroscope-Simon <simon.kollaart@agroscope.admin.ch>
2022-12-18 21:13:10 +0100

    txt file

--- script.R
+++ script.R
@@ -2,6 +2,7 @@ library(tidyselect)
 library(tidytext)
 library(tidyverse)
 library(gutenbergr)
+library(here)
 
 
 view(gutenberg_metadata)
@@ -32,10 +33,21 @@ gutenberg_metadata %>%
 romeo <- gutenberg_works(title == "Romeo and Juliet") %>%
   gutenberg_download(meta_fields = "title")
 
-# load data with ID
-promised_day <- gutenberg_works(gutenberg_id == "16941") %>%
-  gutenberg_download(meta_fields = "gutenberg_id")
+  # load data with ID
+
+my_mirror = "http://gutenberg.readingroo.ms/"
+
+promised_day <- gutenberg_works(gutenberg_id == "16523") %>%
+  gutenberg_download(meta_fields = "gutenberg_id", mirror = my_mirror)
 
 # simultaneous downloads
 texts <- gutenberg_download(c(768, 1260), meta_fields = "title", 
                             mirror = "http://mirrors.xmission.com/gutenberg/")
+
+
+my_mirror <- "http://mirrors.xmission.com/gutenberg/"
+thoreau_text <- gutenberg_works(author == "Thoreau, Henry David") %>%
+  gutenberg_download(meta_fields = "title", mirror = my_mirror)
+
+
+some_answered_questions <- read.delim(here("data", "some_answered_questions.txt"),sep="\t" )
